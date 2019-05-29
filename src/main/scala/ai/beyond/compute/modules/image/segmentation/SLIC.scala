af62d95bd161d0e63e8df4f704e8bfe2fa18969d
George K. Paloulian <gpaloulian@beyond.ai>
2019-05-28 23:03:02 -0700

    added adjust centers to low contrast, attempts to move centers away from hard lines/edges

--- src/main/scala/ai/beyond/compute/modules/image/segmentation/SLIC.scala
+++ src/main/scala/ai/beyond/compute/modules/image/segmentation/SLIC.scala
@@ -7,15 +7,12 @@ import org.nd4j.linalg.indexing.conditions._
 import org.nd4j.linalg.indexing.{BooleanIndexing, NDArrayIndex}
 import org.nd4j.linalg.api.ops.impl.reduce.longer.MatchCondition
 
-import scala.collection.parallel.mutable.ParArray
-
-
 class SLIC (
        matrix: INDArray,
        dimensions: (Int, Int, Int, Int),
        superPixelSize: Int = 15,
-       compactness: Float = .1f,
-       maxIteration: Int = 2,
+       compactness: Float = .01f,
+       maxIteration: Int = 3,
        minSuperSize: Int = 1,
        centerDelta: (Float, Float, Float) = (2f, 2f, 2f)
     ) (implicit logger: akka.event.LoggingAdapter) {
@@ -134,16 +131,18 @@ class SLIC (
       (x_s, y_s, z_s)
     }
 
-    log.info("Super Centers Initialized [{}].", centersGrid.length)
+    log.info("Super Centers Initialized [{}]", centersGrid.length)
 
     log.info("Optimizing Grid Super Centers...")
-
     // Optimize the centers and get them back as an array
     val optimumCentersGrid = adjustCentersAwayFromEmptyRegions(centersGrid)
+    log.info("Optimized Grid Super Centers [{}]", optimumCentersGrid.length)
 
-    log.info("Optimized Grid Super Centers [{}].", optimumCentersGrid.length)
+    log.info("Perturbing Super Centers to Low Contrast...")
+    val perturbedCentersGrid = adjustSuperCentersToLowContrast(optimumCentersGrid)
+    log.info("Perturbed Super Centers to Low Contrast")
 
-    optimumCentersGrid // Return optimum placed centers
+    perturbedCentersGrid // Return optimum placed centers
   }
 
   /**
@@ -164,7 +163,8 @@ class SLIC (
     var markedForAddition: IndexedSeq[(Int, Int, Int)] = IndexedSeq()
 
     // Loop through each center
-    for (ci <- centers.indices) {
+    //for (ci <- centers.indices) {
+    for (ci <- (0 until centers.length).par) {
 
       // Get the current center
       val c = centers(ci)
@@ -283,6 +283,137 @@ class SLIC (
   }
 
   /**
+    * Super pixel centers need to avoid initialization on a clear edge. Perturb the centers
+    * to the lowest 'discrete' gradient in a small radius/cube around the original init.
+    * @param centers An Array of (int,int,int) representing grid centers in matrix
+    * @param adjustBy The buffer of the sphere around center point to search for low contrast
+    * @return Modified list of Centers as [(int, int, int)]
+    */
+  private def adjustSuperCentersToLowContrast(
+                         centers: Array[(Int, Int, Int)],
+                         adjustBy: Int = 3): Array[(Int, Int, Int)] = {
+
+    import scala.math.floor
+    import org.nd4j.linalg.ops.transforms.Transforms.pow
+    import org.nd4j.linalg.ops.transforms.Transforms.sqrt
+    import org.nd4j.linalg.factory.Broadcast
+
+    // Loop through each current center
+    centers.map( c => {
+      // Existing current center
+      val xC: Int = c._1
+      val yC: Int = c._2
+      val zC: Int = c._3
+
+      // Scores and best move for current center
+      var maxScore: Float = 0f // Help to determine what the max score is for a particular center
+      var bestMove: (Int, Int, Int) = (xC, yC, zC) // The starting best move is no move at all
+
+      // Calc starting and ending positions for x.y.z. around current center with adjustBy
+      // This will define candidate new centers around this existing current center
+      val xStart: Int = if (c._1 - adjustBy < 0) 0 else floor(c._1 - adjustBy).toInt
+      val yStart: Int = if (c._2 - adjustBy < 0) 0 else floor(c._2 - adjustBy).toInt
+      val zStart: Int = if (c._3 - adjustBy < 0) 0 else floor(c._3 - adjustBy).toInt
+      val xEnd: Int = if (c._1 + adjustBy > xDimSize) xDimSize else floor(c._1 + adjustBy).toInt
+      val yEnd: Int = if (c._2 + adjustBy > yDimSize) yDimSize else floor(c._2 + adjustBy).toInt
+      val zEnd: Int = if (c._3 + adjustBy > zDimSize) zDimSize else floor(c._3 + adjustBy).toInt
+
+      // Generate points for all potentially new centers around existing center
+      val potentialNewCenters = for {
+        x_s <- xStart until xEnd
+        y_s <- yStart until yEnd
+        z_s <- zStart until zEnd
+      } yield {
+        (x_s, y_s, z_s)
+      }
+
+      // Loop through in parallel and calculate values for each new center
+      // Determine which center is best
+      potentialNewCenters.par.foreach(newCenter => {
+
+        val newCenterVoxelCoords: INDArray = matrix.get(
+          NDArrayIndex.point(newCenter._1),
+          NDArrayIndex.point(newCenter._2),
+          NDArrayIndex.point(newCenter._3),
+          NDArrayIndex.interval(0,3) // Indices of coordinates stored in vector
+        )
+        val newCenterVoxelFeatures: INDArray = matrix.get(
+          NDArrayIndex.point(newCenter._1),
+          NDArrayIndex.point(newCenter._2),
+          NDArrayIndex.point(newCenter._3),
+          NDArrayIndex.interval(3,sDimSize) // Indices of actual data features in vector
+        )
+
+        // Calculate starting and end indices for voxels surrounding the candidate
+        val ncxStart: Int = if (newCenter._1 - 1 < 0) 0 else floor(newCenter._1 - 1).toInt
+        val ncyStart: Int = if (newCenter._2 - 1 < 0) 0 else floor(newCenter._2 - 1).toInt
+        val nczStart: Int = if (newCenter._3 - 1 < 0) 0 else floor(newCenter._3 - 1).toInt
+        val ncxEnd: Int = if (newCenter._1 + 1 > xDimSize) xDimSize else floor(newCenter._1 + 1).toInt
+        val ncyEnd: Int = if (newCenter._2 + 1 > yDimSize) yDimSize else floor(newCenter._2 + 1).toInt
+        val nczEnd: Int = if (newCenter._3 + 1 > zDimSize) zDimSize else floor(newCenter._3 + 1).toInt
+
+        val arrayIndexIntervalsX = NDArrayIndex.interval(ncxStart, ncxEnd)
+        val arrayIndexIntervalsY = NDArrayIndex.interval(ncyStart, ncyEnd)
+        val arrayIndexIntervalsZ = NDArrayIndex.interval(nczStart, nczEnd)
+
+        // get information on all the voxels surrounding this center point
+        val voxelCoords: INDArray = matrix.get(
+          arrayIndexIntervalsX, arrayIndexIntervalsY, arrayIndexIntervalsZ,
+          NDArrayIndex.interval(0,3) // Indices of coordinates stored in vector
+        )
+        val voxelFeatures: INDArray = matrix.get(
+          arrayIndexIntervalsX, arrayIndexIntervalsY, arrayIndexIntervalsZ,
+          NDArrayIndex.interval(3,sDimSize) // Indices of actual data features in vector
+        )
+
+        // broadcast subtraction of center point to range of all voxels
+        // we want to get coord distance of. this is because shapes are not the same
+        // Voxel Coords is a 3D shape surrounding the center point. Center Coords is
+        // a scalar vector since we pull out 4th dimension using ArrayIndex.point.
+        val resultForSubCoords: INDArray = Nd4j.valueArrayOf(voxelCoords.shape(), -1f)
+        val subCoords = Broadcast.sub(
+          voxelCoords, newCenterVoxelCoords, resultForSubCoords, 3)
+
+        // broadcast subtraction of center point to range of all voxels
+        // we want to get feature distance of. this is because shapes are not the same.
+        // Voxel Features is a 3D shape surrounding the center point. Center Features is
+        // a scalar vector since we pull out 4th dimension using ArrayIndex.point.
+        val resultForSubFeatures: INDArray = Nd4j.valueArrayOf(voxelFeatures.shape(), -1f)
+        val subFeatures = Broadcast.sub(
+          voxelFeatures, newCenterVoxelFeatures, resultForSubFeatures, 3)
+
+        // Calculate the distances of coordinates and features. This will give us a matrix
+        // of shape (xEnd-xStart, yEnd-yStart, zEnd-zStart)
+        // calculate the distance of coords with center. multiply by ivtwt is a SLIC implementation
+        val distanceCoord = sqrt(pow(subCoords, 2).sum(3)).muli(ivtwt)
+        // calculate the distance of coords with center
+        val distanceFeatures = sqrt(pow(subFeatures, 2).sum(3))
+        //log.info("Distance Features: \n{}", distanceFeatures)
+
+        // Add the distance measurements for coordinate space and feature together for each voxel
+        val calculatedDistances = distanceCoord.addi(distanceFeatures) // addi is in place addition
+
+        // Replace potential NaNs before proceeding
+        BooleanIndexing.replaceWhere(calculatedDistances,0f, Conditions.isNan)
+
+        val distSum = calculatedDistances.sumNumber().floatValue()
+        log.debug("Distance Sum during perturbing c[{},{},{}] = {}",
+          xC, yC, zC, distSum)
+
+        // If difSum is less than current Max Score then we found a good new
+        // center to replace the current center. Save it and update Max Score
+        if (distSum > maxScore) {
+          bestMove = newCenter
+          maxScore = distSum
+        }
+      })
+
+      // Return the new center point, stored as best move
+      bestMove
+    })
+  }
+
+  /**
     * The main private function that does the SLIC algorithm
     * @return 3D INDArray of integers indicating segment labels
     */
@@ -311,7 +442,8 @@ class SLIC (
       // Basically this goes through and assigns voxels to clusters
       // based on distance calculations according to SLIC
       val t0_clusterAssign = System.nanoTime()
-      for (ci <- (0 until centerCoords.length).toParArray) {
+      //for (ci <- centerCoords.indices) {
+      for (ci <- (0 until centerCoords.length).par) {
 
         // Get the current center
         val c = centerCoords(ci)
@@ -336,7 +468,7 @@ class SLIC (
           NDArrayIndex.point(c._1),
           NDArrayIndex.point(c._2),
           NDArrayIndex.point(c._3),
-          NDArrayIndex.interval(3,6) // Indices of actual data features in vector
+          NDArrayIndex.interval(3,sDimSize) // Indices of actual data features in vector
         )
 
         val cAroundShape = Array(xEnd - xStart, yEnd - yStart, zEnd - zStart).map(i=>i.toLong)
@@ -352,7 +484,7 @@ class SLIC (
         )
         val voxelFeatures: INDArray = matrix.get(
           arrayIndexIntervalsX, arrayIndexIntervalsY, arrayIndexIntervalsZ,
-          NDArrayIndex.interval(3,6) // Indices of actual data features in vector
+          NDArrayIndex.interval(3,sDimSize) // Indices of actual data features in vector
         )
 
         // broadcast subtraction of center point to range of all voxels
@@ -455,7 +587,12 @@ class SLIC (
     * isolated "islands" to the best adjacent superpixel.
     */
   private def enforceConnectivity(): Unit = {
-    // TODO: Implement this
+
+    log.info("Enforcing Connectivity...")
+
+    // TODO: Implement
+
+    log.info("Enforced Connectivity")
   }
 
   /**
